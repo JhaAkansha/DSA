bc43872fc62c28384878f18be2f0f9bfdfce9521
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2024-11-05 14:25:05 +0530

    Create RemoveDuplicateLetters.java

---
+++ Strings/RemoveDuplicateLetters.java
@@ -0,0 +1,31 @@
+/*
+Given a string s, remove duplicate letters so that every letter appears once and only once. You must make sure your result is the smallest in lexicographical order among all possible results.
+*/
+
+public class Solution {
+    public String removeDuplicateLetters(String s) {
+        Stack<Character> stack = new Stack<>();
+        Set<Character> seen = new HashSet<>();
+        Map<Character, Integer> lastOcc = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            lastOcc.put(s.charAt(i), i);
+        }
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!seen.contains(c)) {
+                while (!stack.isEmpty() && c < stack.peek() && i < lastOcc.get(stack.peek())) {
+                    seen.remove(stack.pop());
+                }
+                seen.add(c);
+                stack.push(c);
+            }
+        }
+        
+        StringBuilder result = new StringBuilder();
+        for (char c : stack) {
+            result.append(c);
+        }
+        return result.toString();
+    }
+}
