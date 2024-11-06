6f106b2cf435a7e4fdbf3cb0eef9ae7893f66b2b
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2024-11-06 11:33:22 +0530

    Create BuildAnArray.java

---
+++ Stack/BuildAnArray.java
@@ -0,0 +1,23 @@
+/*
+Use the two stack operations to make the numbers in the stack (from the bottom to the top) equal to target. You should follow the following rules:
+
+    If the stream of the integers is not empty, pick the next integer from the stream and push it to the top of the stack.
+    If the stack is not empty, pop the integer at the top of the stack.
+    If, at any moment, the elements in the stack (from the bottom to the top) are equal to target, do not read new integers from the stream and do not do more operations on the stack.
+*/
+
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> res = new ArrayList<>();
+        var j = 0;      // сounter for target
+        var i = 1;       // counter for n
+
+        while(j < target.length) {
+            res.add("Push");
+            if (target[j] != i) res.add("Pop");
+            else j++;
+            i++;
+        }
+        return res ;   
+    }
+}
