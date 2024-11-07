6d6cc57a6179fe3f483c9f294b1842bb01899c69
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2024-11-07 12:09:01 +0530

    Create NumberOfIslands.java

---
+++ Queue/NumberOfIslands.java
@@ -0,0 +1,34 @@
+/*
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+*/
+
+public class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int m = grid.length, n = grid[0].length;
+        int count = 0;
+        for (int i = 0; i < m ; i ++) {
+            for (int j = 0; j < n; j ++) {
+                if (grid[i][j] == '1') {
+                    count ++;
+                    dfs(grid, m, n, i, j);
+                }
+            }
+        }
+        return count;
+    }
+    private void dfs(char[][] grid, int m, int n, int i, int j) {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != '1') {
+            return;
+        }
+        grid[i][j] = 'X';
+        dfs(grid, m, n, i + 1, j);
+        dfs(grid, m, n, i - 1, j);
+        dfs(grid, m, n, i, j + 1);
+        dfs(grid, m, n, i, j - 1);
+    }
+}
