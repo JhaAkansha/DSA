4ce8346a49afac8b0c15a6467c9eb2481a575554
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2024-11-07 12:10:33 +0530

    Create PerfectSquares.java

---
+++ Queue/PerfectSquares.java
@@ -0,0 +1,30 @@
+/*
+Given an integer n, return the least number of perfect square numbers that sum to n.
+
+A perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself. For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
+*/
+
+class Solution {
+    public int numSquares(int n) {
+	int sqrt = (int) Math.sqrt(n);
+
+	if (sqrt * sqrt == n) // Perfect square
+		return 1;
+
+	while (n % 4 == 0) // 4^a (8b + 7)
+		n = n / 4;
+
+	if (n % 8 == 7)
+		return 4;
+
+	for (int i = 1; i * i <= n; i++) { // Sum of two perfect squares
+		int square = i * i;
+		int base = (int) Math.sqrt(n - square);
+
+		if (base * base == n - square)
+			return 2;
+	}
+
+	return 3;
+}
+}
