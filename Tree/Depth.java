bd9f3566c3d3c20e5793877ce551d04da3b8ec81
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2024-11-08 12:07:01 +0530

    Create Depth.java

---
+++ Tree/Depth.java
@@ -0,0 +1,19 @@
+/*
+Find the depth of a tree node.
+*/
+
+public int depth(TreeNode node, TreeNode key) {
+		if (node == null) return -1;
+		int distance = -1;
+		if (node.data == key.data) 
+			distance =  distance+1;
+		int leftDepth = depth(node.left, key);
+		int rightDepth= depth(node.right, key);
+		if (leftDepth >= 0) {
+			distance =  leftDepth + 1;
+		}
+		if (rightDepth >= 0) {
+			distance = rightDepth + 1;
+		}
+		return distance;
+	}
