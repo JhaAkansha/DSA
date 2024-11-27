59aa38a2f5d54262d92ef2ecac8cb88b8fad5c5a
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2024-11-27 20:58:47 +0530

    Create ContainsDuplicates.java

---
+++ Arrays/ContainsDuplicates.java
@@ -0,0 +1,32 @@
+/*
+You are given an integer array nums and two integers indexDiff and valueDiff.
+
+Find a pair of indices (i, j) such that:
+
+    1. i != j,
+    2. abs(i - j) <= indexDiff.
+    3. abs(nums[i] - nums[j]) <= valueDiff, and
+
+Return true if such pair exists or false otherwise.
+*/
+
+public class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        if (k < 1 || t < 0) return false;
+        Map<Long, Long> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            long remappedNum = (long) nums[i] - Integer.MIN_VALUE;
+            long bucket = remappedNum / ((long) t + 1);
+            if (map.containsKey(bucket)
+                    || (map.containsKey(bucket - 1) && remappedNum - map.get(bucket - 1) <= t)
+                        || (map.containsKey(bucket + 1) && map.get(bucket + 1) - remappedNum <= t))
+                            return true;
+            if (map.entrySet().size() >= k) {
+                long lastBucket = ((long) nums[i - k] - Integer.MIN_VALUE) / ((long) t + 1);
+                map.remove(lastBucket);
+            }
+            map.put(bucket, remappedNum);
+        }
+        return false;
+    }
+}
