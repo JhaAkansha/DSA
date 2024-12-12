9be6378e8de6923c9d690f8731b273a82749ca94
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2024-12-12 23:50:06 +0530

    Create MaxSubArray.java

---
+++ DP/MaxSubArray.java
@@ -0,0 +1,17 @@
+/*
+Given an integer array nums, find the subarray with the largest sum, and return its sum.
+*/
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int local_max = 0;
+        int global_max = Integer.MIN_VALUE;
+        int n = nums.length;
+        for (int i = 0; i < n; i++){
+            local_max = Math.max(nums[i], nums[i] + local_max);
+            if (local_max > global_max) {
+                global_max = local_max;
+            }
+        }
+        return global_max;
+    }
+}
