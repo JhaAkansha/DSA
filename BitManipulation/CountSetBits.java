4d7f46c7743e0cf3f6ee2773de4973a1f19cc98e
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-01-11 00:15:16 +0530

    Create CountSetBits.java

---
+++ BitManipulation/CountSetBits.java
@@ -0,0 +1,13 @@
+/*
+Given a positive integer n, write a function that returns the number of set bits in its binary representation (also known as the Hamming weight).
+*/
+class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+        while ( n != 0) {
+            if (n%2 == 1) count++;
+            n = n/2;
+        }
+        return count;
+    }
+}
