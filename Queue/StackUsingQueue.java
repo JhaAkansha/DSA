ba861607c26fd2b86250a6b811c8a4ed87998e95
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-02-15 23:15:40 +0530

    Update StackUsingQueue.java

--- Queue/StackUsingQueue.java
+++ Queue/StackUsingQueue.java
@@ -40,11 +40,11 @@ class MyStack {
     }
 }
 
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack obj = new MyStack();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.top();
- * boolean param_4 = obj.empty();
- */
+/*
+  Your MyStack object will be instantiated and called as such:
+  MyStack obj = new MyStack();
+  obj.push(x);
+  int param_2 = obj.pop();
+  int param_3 = obj.top();
+  boolean param_4 = obj.empty();
+*/
