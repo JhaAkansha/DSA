b7f072abb404fc5ca91cbb755d5d97087fa5266c
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-02-15 23:15:11 +0530

    Update StackUsingQueue.java

--- Queue/StackUsingQueue.java
+++ Queue/StackUsingQueue.java
@@ -1,15 +1,11 @@
 /*
 Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
-
 Implement the MyStack class:
-
     1. void push(int x) Pushes element x to the top of the stack.
     2. int pop() Removes the element on the top of the stack and returns it.
     3. int top() Returns the element on the top of the stack.
     4. boolean empty() Returns true if the stack is empty, false otherwise.
-
 */
-
 class MyStack {
     Queue<Integer> q;
     int top;
