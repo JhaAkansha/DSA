1bcbef9647833f4914627bf9b2a394130e7211ad
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-02-18 23:07:42 +0530

    Update KeysAndRooms.java

--- Graph/KeysAndRooms.java
+++ Graph/KeysAndRooms.java
@@ -10,19 +10,16 @@ class Solution {
         seen[0] = true;
         Stack<Integer> stack = new Stack();
         stack.push(0);
-
-        //At the beginning, we have a todo list "stack" of keys to use.
-        //'seen' represents at some point we have entered this room.
-        while (!stack.isEmpty()) { // While we have keys...
-            int node = stack.pop(); // Get the next key 'node'
-            for (int nei: rooms.get(node)) // For every key in room # 'node'...
-                if (!seen[nei]) { // ...that hasn't been used yet
-                    seen[nei] = true; // mark that we've entered the room
-                    stack.push(nei); // add the key to the todo list
+        while (!stack.isEmpty()) { 
+            int node = stack.pop(); 
+            for (int nei: rooms.get(node))
+                if (!seen[nei]) {
+                    seen[nei] = true;
+                    stack.push(nei);
                 }
         }
 
-        for (boolean v: seen)  // if any room hasn't been visited, return false
+        for (boolean v: seen)
             if (!v) return false;
         return true;
     }
