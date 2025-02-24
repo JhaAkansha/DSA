ef792bc8f8478187e0207c795e0c2cdacf2a577a
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-02-24 22:43:22 +0530

    Update ValidParentheses.java

--- Stack/ValidParentheses.java
+++ Stack/ValidParentheses.java
@@ -1,13 +1,10 @@
 /*
 Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-
 An input string is valid if:
-
-    Open brackets must be closed by the same type of brackets.
-    Open brackets must be closed in the correct order.
-    Every close bracket has a corresponding open bracket of the same type.
-  */
-
+    1. Open brackets must be closed by the same type of brackets.
+    2. Open brackets must be closed in the correct order.
+    3. Every close bracket has a corresponding open bracket of the same type.
+*/
 class Solution {
     public boolean isValid(String s) {
         Stack <Character> stack = new Stack<>();
