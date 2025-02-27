dbbe618b2d3470e3da4e6c655f00665b62429688
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-02-27 18:11:54 +0530

    Create HouseRobber.java

---
+++ DP/HouseRobber.java
@@ -0,0 +1,17 @@
+/*
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+*/
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 0) return 0;
+        int prev1 = 0;
+        int prev2 = 0;
+        for (int num : nums) {
+            int tmp = prev1;
+            prev1 = Math.max(prev2 + num, prev1);
+            prev2 = tmp;
+        }
+        return prev1;
+    }
+}
