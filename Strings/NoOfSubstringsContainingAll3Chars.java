01244445972bd9b837f6e26644c892d793338d80
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-03-01 09:05:07 +0530

    Create NoOfSubstringsContainingAll3Chars.java

---
+++ Strings/NoOfSubstringsContainingAll3Chars.java
@@ -0,0 +1,16 @@
+/*
+Given a string s consisting only of characters a, b and c.
+Return the number of substrings containing at least one occurrence of all these characters a, b and c.
+*/
+class Solution {
+    public int numberOfSubstrings(String s) {
+        int count[] = {0, 0, 0}, res = 0 , i = 0, n = s.length();
+        for (int j = 0; j < n; ++j) {
+            ++count[s.charAt(j) - 'a'];
+            while (count[0] > 0 && count[1] > 0 && count[2] > 0)
+                --count[s.charAt(i++) - 'a'];
+            res += i;
+        }
+        return res;
+    }
+}
