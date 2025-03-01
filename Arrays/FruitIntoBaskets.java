e6918db7a06ecf187a10a53a4edf02adf653a643
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-03-01 13:19:29 +0530

    Create FruitIntoBaskets.java

---
+++ Arrays/FruitIntoBaskets.java
@@ -0,0 +1,25 @@
+/*
+You are visiting a farm that has a single row of fruit trees arranged from left to right. The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.
+
+You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow:
+
+    You only have two baskets, and each basket can only hold a single type of fruit. There is no limit on the amount of fruit each basket can hold.
+    Starting from any tree of your choice, you must pick exactly one fruit from every tree (including the start tree) while moving to the right. The picked fruits must fit in one of your baskets.
+    Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
+
+Given the integer array fruits, return the maximum number of fruits you can pick.
+*/
+class Solution {
+        public int totalFruit(int[] tree) {
+        Map<Integer, Integer> count = new HashMap<>();
+        int i = 0, j;
+        for (j = 0; j < tree.length; ++j) {
+            count.put(tree[j], count.getOrDefault(tree[j], 0) + 1);
+            if (count.size() > 2) {
+                count.put(tree[i], count.get(tree[i]) - 1);
+                count.remove(tree[i++], 0);
+            }
+        }
+        return j - i;
+    }
+}
