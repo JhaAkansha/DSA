aff71d9b790aaf10677a2a345497b5dc37d2c15f
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-03-13 22:31:37 +0530

    Update BinaryTreePaths.java

--- Tree/BinaryTreePaths.java
+++ Tree/BinaryTreePaths.java
@@ -1,13 +1,13 @@
 /*
 Given the root of a binary tree, return all root-to-leaf paths in any order.
-*/
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+Definition for a binary tree node.
+ class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+*/
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
         res = []
