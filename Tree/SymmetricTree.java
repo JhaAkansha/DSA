5e06dd185c2a75fc441205c43edcd0a74114778b
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-03-13 22:38:27 +0530

    Update SymmetricTree.java

--- Tree/SymmetricTree.java
+++ Tree/SymmetricTree.java
@@ -1,21 +1,21 @@
 /*
 Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
 */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+/*
+  Definition for a binary tree node.
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+*/
 class Solution {
     public boolean check(TreeNode p,TreeNode q){
         if(p==null && q==null) return true;
