fb5877d44d1f5f74bbb90943b0fb267c9a641d52
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-03-13 22:33:23 +0530

    Update CountCompleteTreeNode.java

--- Tree/CountCompleteTreeNode.java
+++ Tree/CountCompleteTreeNode.java
@@ -3,20 +3,20 @@ Given the root of a complete binary tree, return the number of the nodes in the 
 According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
 Design an algorithm that runs in less than O(n) time complexity.
 */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
+/*
+ Definition for a binary tree node.
+ public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
  */
 class Solution {
     public int countNodes(TreeNode root) {
