673503ee10e41999c04493fd4a3ef905181e0fe8
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-03-15 21:27:43 +0530

    Update FirstAndLastPos.py

--- Searching&Sorting/FirstAndLastPos.py
+++ Searching&Sorting/FirstAndLastPos.py
@@ -1,9 +1,7 @@
 '''
 Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
-
 If target is not found in the array, return [-1, -1].
 '''
-
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         lst = [-1,-1]
