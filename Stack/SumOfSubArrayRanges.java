388ac6f394b1ef188818f94f679163402152a72e
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-03-15 21:24:00 +0530

    Update SumOfSubArrayRanges.java

--- Stack/SumOfSubArrayRanges.java
+++ Stack/SumOfSubArrayRanges.java
@@ -1,11 +1,8 @@
 /*
 You are given an integer array nums. The range of a subarray of nums is the difference between the largest and smallest element in the subarray.
-
 Return the sum of all subarray ranges of nums.
-
 A subarray is a contiguous non-empty sequence of elements within an array.
 */
-
 class Solution {
         public long subArrayRanges(int[] A) {
         int n = A.length, j, k;
@@ -17,18 +14,15 @@ class Solution {
                 j = s.pop();
                 k = s.isEmpty() ? -1 : s.peek();
                 res -= (long)A[j] * (i - j) * (j - k);
-
             }
             s.push(i);
         }
-        
         s.clear();
         for (int i = 0; i <= n; i++) {
             while (!s.isEmpty() && A[s.peek()] < (i == n ? Integer.MAX_VALUE : A[i])) {
                 j = s.pop();
                 k = s.isEmpty() ? -1 : s.peek();
                 res += (long)A[j] * (i - j) * (j - k);
-
             }
             s.push(i);
         }
