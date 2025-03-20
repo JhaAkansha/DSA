f4d45e525d2c16a6fb24f972fb7fe1637e05f8a8
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-03-20 23:18:06 +0530

    Update DiagonalDifference.py

--- Matrix/DiagonalDifference.py
+++ Matrix/DiagonalDifference.py
@@ -1,13 +1,6 @@
 '''
 Given a square matrix, calculate the absolute difference between the sums of its diagonals.
 '''
-
-import math
-import os
-import random
-import re
-import sys
-
 # Complete the 'diagonalDifference' function below.
 # The function is expected to return an INTEGER.
 # The function accepts 2D_INTEGER_ARRAY arr as parameter.
