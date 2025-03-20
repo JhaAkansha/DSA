2aa759fc10fdfd909f04628297e08de08471032c
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-03-20 23:18:46 +0530

    Update Search.java

--- Matrix/Search.java
+++ Matrix/Search.java
@@ -2,7 +2,6 @@
 You are given an m x n integer matrix matrix with the following two properties:
   1. Each row is sorted in non-decreasing order.
   2. The first integer of each row is greater than the last integer of the previous row.
-
 Given an integer target, return true if target is in matrix or false otherwise.
 */
 
