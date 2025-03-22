3846141d7b8d7e8037a01cf9641f2cdb0c22404c
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-03-22 23:09:30 +0530

    Update ReverseNodesInKGroups.java

--- LinkedList/ReverseNodesInKGroups.java
+++ LinkedList/ReverseNodesInKGroups.java
@@ -3,7 +3,7 @@ Given the head of a linked list, reverse the nodes of the list k at a time, and 
 k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
 You may not alter the values in the list's nodes, only nodes themselves may be changed.
 */
-/**
+/*
  Definition for singly-linked list.
  public class ListNode {
       int val;
@@ -18,23 +18,18 @@ class Solution {
         ListNode dummy = new ListNode(0);
         dummy.next = head;
         ListNode prevGroupTail = dummy;
-
         while (head != null) {
             ListNode groupStart = head;
             ListNode groupEnd = getGroupEnd(head, k);
-
             if (groupEnd == null)
                 break;
-
             prevGroupTail.next = reverseList(groupStart, groupEnd.next);
             prevGroupTail = groupStart;
             head = prevGroupTail.next;
         }
-
         ListNode newHead = dummy.next;
         return newHead;
     }
-
     private ListNode getGroupEnd(ListNode head, int k) {
         while (head != null && --k > 0)
             head = head.next;
