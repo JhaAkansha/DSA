95863d5abf6a5269a3ea19748eae14b14400a6af
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-03-22 23:10:08 +0530

    Update ReverseLinkedListII.java

--- LinkedList/ReverseLinkedListII.java
+++ LinkedList/ReverseLinkedListII.java
@@ -1,15 +1,15 @@
 /*
 Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
 */
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+/*
+  Definition for singly-linked list.
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
