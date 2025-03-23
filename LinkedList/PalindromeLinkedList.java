727e7b291329822673f13bba423283eb04a97e6a
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-03-23 20:50:30 +0530

    Update PalindromeLinkedList.java

--- LinkedList/PalindromeLinkedList.java
+++ LinkedList/PalindromeLinkedList.java
@@ -2,15 +2,15 @@
 Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
 */
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+/*
+  Definition for singly-linked list.
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
