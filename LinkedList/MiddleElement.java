49e2d0b1759760eff22f569056e80d35c968e5e5
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-03-23 20:50:08 +0530

    Update MiddleElement.java

--- LinkedList/MiddleElement.java
+++ LinkedList/MiddleElement.java
@@ -4,15 +4,15 @@ Given the head of a singly linked list, return the middle node of the linked lis
 If there are two middle nodes, return the second middle node.
 */
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+/*
+  Definition for singly-linked list.
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
