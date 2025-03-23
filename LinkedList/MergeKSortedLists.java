b072fc0151c1965c177806ad1d98793fb1e67776
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-03-23 20:49:19 +0530

    Update MergeKSortedLists.java

--- LinkedList/MergeKSortedLists.java
+++ LinkedList/MergeKSortedLists.java
@@ -3,15 +3,15 @@ You are given an array of k linked-lists lists, each linked-list is sorted in as
 
 Merge all the linked-lists into one sorted linked-list and return it.
 */
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+/*
+  Definition for singly-linked list.
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2)
