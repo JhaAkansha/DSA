16cc48713c8ceee080190373c5920bd976b69c51
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-03-31 21:59:34 +0530

    Update CloneGraph.java

--- Graph/CloneGraph.java
+++ Graph/CloneGraph.java
@@ -6,6 +6,7 @@ Test case format:
 For simplicity, each node's value is the same as the node's index (1-indexed). For example, the first node with val == 1, the second node with val == 2, and so on. The graph is represented in the test case using an adjacency list.
 An adjacency list is a collection of unordered lists used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
 The given node will always be the first node with val = 1. You must return the copy of the given node as a reference to the cloned graph.
+
 // Definition for a Node.
 class Node {
     public int val;
@@ -36,8 +37,7 @@ class Solution {
             else{
                 copy.neighbors.add(visited[n.val]);
             }
-        }
-        
+        }   
     }
     public Node cloneGraph(Node node) {
         if(node == null) return null; 
