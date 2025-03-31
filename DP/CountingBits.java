ee330b50cdfaaedfb9b4660a222b87b74ea79d49
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-03-31 22:01:00 +0530

    Update CountingBits.java

--- DP/CountingBits.java
+++ DP/CountingBits.java
@@ -1,6 +1,7 @@
 /*
 Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
 */
+
 class Solution {
     public int numberOfOnes(int a) {
         int temp = 0;
