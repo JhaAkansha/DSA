5f287277c3895e89d1c2d6eb34788336a40ee730
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-04-22 22:18:44 +0530

    Update BirthdayCakeCandles.py

--- Arrays/BirthdayCakeCandles.py
+++ Arrays/BirthdayCakeCandles.py
@@ -10,15 +10,11 @@ import random
 import re
 import sys
 
-#
 # Complete the 'birthdayCakeCandles' function below.
-#
 # The function is expected to return an INTEGER.
 # The function accepts INTEGER_ARRAY candles as parameter.
-#
 
 def birthdayCakeCandles(candles):
-    # Write your code here
     large = candles[0]
     for i in range (0,candles_count):
         if (candles[i]>large):
@@ -31,13 +27,8 @@ def birthdayCakeCandles(candles):
             
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     candles_count = int(input().strip())
-
     candles = list(map(int, input().rstrip().split()))
-
     result = birthdayCakeCandles(candles)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
