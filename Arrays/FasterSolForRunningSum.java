07e426559891f11c909508e0b4552eb145f82906
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-05-02 17:24:59 +0530

    Update FasterSolForRunningSum.java

--- Arrays/FasterSolForRunningSum.java
+++ Arrays/FasterSolForRunningSum.java
@@ -1,8 +1,8 @@
 /*
 Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
-
 Return the running sum of nums.
 */
+
 class Solution {
     public int[] runningSum(int[] nums) {
         int[] res = new int[nums.length];
