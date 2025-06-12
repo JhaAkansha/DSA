517d5b16ff7dc8141592791e327e999df4141318
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-06-12 15:42:33 +0530

    Update LongestIncreasingSubstring.java

--- DP/LongestIncreasingSubstring.java
+++ DP/LongestIncreasingSubstring.java
@@ -6,11 +6,9 @@ public class Solution {
         if (nums == null || nums.length == 0) {
             return 0;
         }
-
         int n = nums.length;
         int[] dp = new int[n];
         Arrays.fill(dp, 1);
-
         for (int i = 1; i < n; ++i) {
             for (int j = 0; j < i; ++j) {
                 if (nums[i] > nums[j]) {
@@ -18,7 +16,6 @@ public class Solution {
                 }
             }
         }
-
         int maxLength = Arrays.stream(dp).max().orElse(0);
         return maxLength;
     }
