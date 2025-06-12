db1a46fd000d20c5ec21c4786c2e6894c237662a
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-06-12 15:33:40 +0530

    Update InsertDeleteGetRandom.java

--- Arrays/InsertDeleteGetRandom.java
+++ Arrays/InsertDeleteGetRandom.java
@@ -1,20 +1,16 @@
 /*
 Implement the RandomizedSet class:
-
     RandomizedSet() Initializes the RandomizedSet object.
     bool insert(int val) Inserts an item val into the set if not present. Returns true if the item was not present, false otherwise.
     bool remove(int val) Removes an item val from the set if present. Returns true if the item was present, false otherwise.
     int getRandom() Returns a random element from the current set of elements (it's guaranteed that at least one element exists when this method is called). Each element must have the same probability of being returned.
-
 You must implement the functions of the class such that each function works in average O(1) time complexity.
 */
 class RandomizedSet {
     private Set<Integer> RandomizedSet;
-
     public RandomizedSet() {
         RandomizedSet = new HashSet<>();
     }
-    
     public boolean insert(int val) {
         if (!RandomizedSet.contains(val)) {
             RandomizedSet.add(val);
@@ -22,7 +18,6 @@ class RandomizedSet {
         }
         return false;
     }
-    
     public boolean remove(int val) {
         if (RandomizedSet.contains(val)) {
             RandomizedSet.remove(val);
@@ -30,7 +25,6 @@ class RandomizedSet {
         }
         return false;
     }
-    
     public int getRandom() {
         int size = RandomizedSet.size();
         int item = new Random().nextInt(size);
@@ -44,10 +38,10 @@ class RandomizedSet {
     }
 }
 
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet obj = new RandomizedSet();
- * boolean param_1 = obj.insert(val);
- * boolean param_2 = obj.remove(val);
- * int param_3 = obj.getRandom();
+/*
+ Your RandomizedSet object will be instantiated and called as such:
+ RandomizedSet obj = new RandomizedSet();
+ boolean param_1 = obj.insert(val);
+ boolean param_2 = obj.remove(val);
+ int param_3 = obj.getRandom();
  */
