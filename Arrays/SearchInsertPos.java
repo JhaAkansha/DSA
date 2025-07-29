910100209e665982c1d31e04cacdb8b57876354a
JhaAkansha <akanshajha47@gmail.com>
2025-07-29 17:29:17 +0530

    Search Insert Position

--- Arrays/SearchInsertPos.java
+++ Arrays/SearchInsertPos.java
@@ -3,3 +3,18 @@ Given a sorted array of distinct integers and a target value, return the index i
 
 You must write an algorithm with O(log n) runtime complexity. */
 
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+
+        while (left <= right) {
+            int mid = left + (right-left)/2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) right = mid-1;
+            else left = mid+1;
+        }
+
+        return left;
+    }
+}
