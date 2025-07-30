8b71e04d5d7b952fc249d9a2d10f080f1e9ff73b
JhaAkansha <akanshajha47@gmail.com>
2025-07-30 17:32:31 +0530

    Calculate x power n

--- Arrays/Pow(x,n).py
+++ Arrays/Pow(x,n).py
@@ -1,3 +1,21 @@
 '''
 Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
 '''
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        prod = 1
+        power = abs(n)
+        if (n == 0):
+            return prod
+        else:
+            while (power > 0):
+                if (power % 2 == 1):
+                    prod = prod*x
+                    power-=1
+                elif (power % 2 == 0):
+                    x = x*x
+                    power = power/2
+            if (n>0):
+                return prod
+            else:
+                return (1/prod)
