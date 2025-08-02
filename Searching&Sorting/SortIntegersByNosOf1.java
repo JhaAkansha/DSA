e7ec34b994f37ce6cec75ec0e4d097592e1ab89e
JhaAkansha <akanshajha47@gmail.com>
2025-08-02 17:47:03 +0530

    Edit solution

--- Searching&Sorting/SortIntegersByNosOf1.java
+++ Searching&Sorting/SortIntegersByNosOf1.java
@@ -2,6 +2,7 @@
 You are given an integer array arr. Sort the integers in the array in ascending order by the number of 1's in their binary representation and in case of two or more integers have the same number of 1's you have to sort them in ascending order.
 Return the array after sorting it.
 */
+
 class Solution {
     public int[] sortByBits(int[] arr) {
         //ok so for this enginner rule is applied where each number is updated by a the number itself + number of bits in number*(10001) and then we will sort it and then we will take the modulo.
