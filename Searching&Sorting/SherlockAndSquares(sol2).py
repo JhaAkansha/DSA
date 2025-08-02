4c90c2385b7b91860504b568980c97d9d8e06666
JhaAkansha <akanshajha47@gmail.com>
2025-08-02 17:47:24 +0530

    Edit solution

--- Searching&Sorting/SherlockAndSquares(sol2).py
+++ Searching&Sorting/SherlockAndSquares(sol2).py
@@ -1,7 +1,9 @@
 '''
 Watson likes to challenge Sherlock's math ability. He will provide a starting and ending value that describe a range of integers, inclusive of the endpoints. Sherlock must determine the number of square integers within that range.
 '''
+
 from math import ceil, floor
+
 def squares(a, b):
     # Write your code here
     res = 0
