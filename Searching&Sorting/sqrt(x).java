3b5b5f5e4f94f72268c75a4a23d9f8eaa9383bc1
JhaAkansha <akanshajha47@gmail.com>
2025-08-02 17:46:15 +0530

    Square root of x

--- Searching&Sorting/sqrt(x).java
+++ Searching&Sorting/sqrt(x).java
@@ -3,3 +3,23 @@ Given a non-negative integer x, return the square root of x rounded down to the 
 You must not use any built-in exponent function or operator.
 For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
 */
+
+class Solution {
+    
+    public int mySqrt(int x) {
+        if (x < 2) return x;
+        
+        int right = x / 2;
+        int left = 1;
+        while (left <= right) {
+            int mid = (right + left) / 2;
+            if ((long)mid*mid > x) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return right;
+    }
+    
+}
