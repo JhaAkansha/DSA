00124decf67ad15429bfcfb050c4944e6fb79149
JhaAkansha <akanshajha47@gmail.com>
2025-08-04 17:54:52 +0530

    Search a 2D matrix

--- Matrix/Search2Dmatrix.java
+++ Matrix/Search2Dmatrix.java
@@ -5,3 +5,25 @@
  Given an integer target, return true if target is in matrix or false otherwise.
  You must write a solution in O(log(m * n)) time complexity.
  */
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int low = 0, high = m*n - 1, mid;
+        while (low <= high) {
+            mid = (low + high)/2;
+            int j = mid % n;
+            int i = (mid - j) / n;
+            if (matrix[i][j] == target) {
+                return true;
+            }
+            else if (matrix[i][j] > target) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
+}
