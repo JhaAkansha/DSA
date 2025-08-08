31a755b9dda5af75fb07ddc3da8193be3e3a53ec
JhaAkansha <akanshajha47@gmail.com>
2025-08-08 18:09:39 +0530

    Find peak element

--- Searching&Sorting/PeakElement.py
+++ Searching&Sorting/PeakElement.py
@@ -7,3 +7,23 @@ You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is al
 
 You must write an algorithm that runs in O(log n) time.
 '''
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        '''
+        left, right = 0, len(nums) - 1
+        while (left <= right):
+            mid = (left + right)//2
+            if (nums[mid] > nums[mid + 1] and nums[mid] > nums[mid - 1]):
+                return mid
+            elif ()
+        '''
+        if (len(nums) == 1):
+            return 0
+        for i in range (1, len(nums) - 1):
+            if (nums[i] > nums[i+1] and nums[i] > nums[i-1]):
+                print(i)
+                return i
+        if (nums[0] > nums[1]):
+                return 0
+        elif (nums[len(nums) - 1] > nums[len(nums) - 2]):
+                return len(nums) - 1
