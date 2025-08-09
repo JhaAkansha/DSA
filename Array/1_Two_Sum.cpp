7dc97f34321b2c0f8fe7692198c5f8e8b8a4e790
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-08-09 22:45:49 +0530

    Add solution for 1. Two Sum

--- Array/1_Two_Sum.cpp
+++ Array/1_Two_Sum.cpp
@@ -40,8 +40,11 @@ Only one valid answer exists.
 Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 */
 
-class Solution:
-    def twoSum(self, nums: List[int], target: int) 
--> List[int]:
-        nmhgytyrfgvbkuyjm
-        
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    dmfsijroikmsd
+};
