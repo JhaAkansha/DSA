d8a88ac3e0857af6b91308dc7988f159b1f5354d
Akansha Jha <94896887+JhaAkansha@users.noreply.github.com>
2025-08-10 19:42:08 +0530

    Add solution for 1. Two sum

--- Array/1_Two_sum.py
+++ Array/1_Two_sum.py
@@ -40,8 +40,7 @@ Follow-up: Can you come up with an algorithm that is less than O(n2) time comp
 """
 
 class Solution:
-    def twoSum(self, nums: List[int], target: 
-int) -> List[int]:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         length = len(nums)
         lst = []
         for i in range (0,length):
